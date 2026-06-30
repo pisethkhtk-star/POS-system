e31e96a9753ba5573c6f115b437b4999367622bc
piseth khoem <pisethkhtk@gmail.com>
2026-06-30 14:15:53 +0700

    chore: move allowedDevOrigins to top level of nextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    // @ts-ignore
-    allowedDevOrigins: ["*"]
-  }
+  // @ts-ignore
+  allowedDevOrigins: ["*"]
 };
 
 export default nextConfig;
